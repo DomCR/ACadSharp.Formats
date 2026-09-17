74c766291b5e2529ad2658ed48768aa76e592607
DomCR <domenech93@gmail.com>
2026-09-17 09:49:12 +0200

    Enhance PdfExporter with export and Add methods
    Implemented Add methods for layouts, block records, and model space in PdfExporter. Export methods now add content and write the PDF. Added constructors for filename and stream. Updated using directives for required namespaces.

--- src/ACadSharp.Formats/Pdf/PdfExporter.cs
+++ src/ACadSharp.Formats/Pdf/PdfExporter.cs
@@ -1,6 +1,8 @@
-﻿using ACadSharp.Formats.Pdf.Core;
+﻿using ACadSharp.Entities;
+using ACadSharp.Formats.Pdf.Core;
 using ACadSharp.Objects;
 using ACadSharp.Tables;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ACadSharp.Formats.Pdf;
@@ -17,28 +19,130 @@ public class PdfExporter : ExporterBase
 
 	private readonly PdfDocument _pdf = new PdfDocument();
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="PdfExporter"/> class with the specified filename.
+	/// </summary>
+	/// <param name="filename">The name of the file to create.</param>
 	public PdfExporter(string filename)
 		: this(File.Create(filename))
 	{
 	}
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="PdfExporter"/> class with the specified stream.
+	/// </summary>
+	/// <param name="stream">The stream to write the PDF to.</param>
 	public PdfExporter(Stream stream)
 		: base(stream)
 	{
 	}
 
+	/// <summary>
+	/// Adds the specified layouts designed as paper space to the PDF document.
+	/// </summary>
+	/// <param name="layouts">The layouts to add.</param>
+	public void Add(IEnumerable<Layout> layouts)
+	{
+		foreach (var layout in layouts)
+		{
+			if (!layout.IsPaperSpace)
+			{
+				continue;
+			}
+
+			this.Add(layout);
+		}
+	}
+
+	/// <summary>
+	/// Adds the specified layout to the PDF document.
+	/// </summary>
+	/// <param name="layout">The layout to add.</param>
+	public void Add(Layout layout)
+	{
+		PdfPage page = this._pdf.Pages.AddPage();
+
+		page.Layout = layout;
+
+		foreach (Entity e in layout.AssociatedBlock.Entities)
+		{
+			if (e is Viewport)
+			{
+				continue;
+			}
+
+			page.Entities.Add(e);
+		}
+
+		foreach (Viewport vp in layout.Viewports)
+		{
+			if (vp.RepresentsPaper)
+			{
+				continue;
+			}
+
+			page.Viewports.Add(vp);
+		}
+	}
+
+	/// <summary>
+	/// Adds the specified block record to the PDF document.
+	/// </summary>
+	/// <param name="block">The block record to add.</param>
+	public void Add(BlockRecord block)
+	{
+		PdfPage page = this._pdf.Pages.AddPage();
+
+		page.Add(block);
+	}
+
+	/// <summary>
+	/// Adds the model space from the specified document to the PDF document.
+	/// </summary>
+	/// <param name="document">The CAD document containing the model space to add.</param>
+	public void AddModelSpace(CadDocument document)
+	{
+		this.Add(document.ModelSpace);
+	}
+
+	/// <summary>
+	/// Adds the paper layouts from the specified document to the PDF document.
+	/// </summary>
+	/// <param name="document">The CAD document containing the layouts to add.</param>
+	public void AddPaperLayouts(CadDocument document)
+	{
+		this.Add(document.Layouts);
+	}
+
+	/// <summary>
+	/// Close the document and save it.
+	/// </summary>
+	public void Close()
+	{
+		using (PdfWriter writer = new PdfWriter(this.stream, this._pdf, this.Configuration))
+		{
+			writer.Write();
+		}
+	}
+
+	/// <inheritdoc/>
 	public override void Export(Layout layout)
 	{
-		throw new System.NotImplementedException();
+		this.Add(layout);
+		this.Close();
 	}
 
+	/// <inheritdoc/>
 	public override void Export(BlockRecord record)
 	{
-		throw new System.NotImplementedException();
+		this.Add(record);
+		this.Close();
 	}
 
+	/// <inheritdoc/>
 	public override void Export(CadDocument document)
 	{
-		throw new System.NotImplementedException();
+		this.AddPaperLayouts(document);
+		this.Close();
 	}
 }
